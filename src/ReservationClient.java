9f877970678ca2dc3406c6627a6fe3dc2c6e96ba
seichen <seichen@purdue.edu>
2019-11-15 19:51:22 -0500

    Server done

--- src/ReservationClient.java
+++ src/ReservationClient.java
@@ -1,2 +1,46 @@
-public class ReservationClient {
+import javax.naming.InvalidNameException;
+import javax.swing.*;
+import java.io.*;
+import java.net.Socket;
+
+public final class ReservationClient implements Runnable {
+    public static void main(String[] args) throws IOException {
+        String hostname;
+        String portString;
+        boolean portCor = false;
+        int port = 0;
+        Socket socket;
+
+
+        hostname = JOptionPane.showInputDialog(null, "Enter the server's hostname: ");
+
+        if (hostname != null) {
+
+            while (!portCor) {
+                portString = JOptionPane.showInputDialog(null, "Enter the server's port: ");
+                try {
+                    port = Integer.parseInt(portString);
+                    portCor = true;
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Incorrect port format");
+                }
+            }
+
+            socket = new Socket(hostname, port);
+        }
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+
+    public boolean isFull(String airline) {
+        return false;
+    }
+
+    public void reserveSpot(String passenger, String airline) {
+
+    }
 }
--- src/ReservationServer.java
+++ src/ReservationServer.java
@@ -1,2 +1,38 @@
-public class ReservationServer {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public final class ReservationServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(0);
+        Socket clientSocket;
+        ArrayList<Thread> t = new ArrayList<>();
+        int connectionCount = 0;
+
+        System.out.printf("<Now serving clients on port %d...>%n", serverSocket.getLocalPort());
+
+        while(true) {
+            try {
+                clientSocket = serverSocket.accept();
+
+            }catch (IOException e) {
+                e.printStackTrace();
+
+                break;
+            }
+
+            t.add(new Thread());
+
+            t.get(connectionCount).start();
+
+            connectionCount++;
+
+            System.out.printf("<Client %d connected...>%n", connectionCount);
+        }
+
+    }
+
+
 }
